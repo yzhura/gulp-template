4bcbedbb75e10e79589586b61ae9b03aaf80e9e2
Yevhen Kalashnikov <kalashnikov0911@gmail.com>
2022-08-05 16:31:46 +0300

    update template

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,35 @@
-jQuery(function() {
-    greeting();
+// Swiper:
+
+function destroySlidersOnResize(selector, width, obj, moreThan) {
+    const init = {
+      ...obj,
+    };
+  
+    const win = window;
+    const sliderSelector = document.querySelector(selector);
+    let swiper = new Swiper(selector, init);
+  
+    const toggleInit = () => {
+      const neededWidth = moreThan ? win.innerWidth >= width : win.innerWidth <= width
+      if (neededWidth) {
+        if (!sliderSelector.classList.contains("swiper-initialized")) {
+          swiper = new Swiper(selector, init);
+        }
+      } else if (sliderSelector.classList.contains("swiper-initialized")) {
+        swiper.destroy();
+      }
+    };
+  
+    ["load", "resize"].forEach((evt) =>
+      win.addEventListener(evt, toggleInit, false)
+    );
+}
+
+destroySlidersOnResize(".me-slider", 960, {
+    spaceBetween: 20,
+  
+    pagination: {
+      el: ".swiper-pagination",
+    },
 });
- 
-let greeting = () => console.log("Hi!");  
+  
